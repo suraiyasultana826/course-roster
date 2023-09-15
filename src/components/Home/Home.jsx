26b81e6939088e1acca2504977bd0525158f40a4
suraiyasultana826 <suraiyasultana826@gmail.com>
2023-09-15 19:20:44 +0600

    fixed a mistake

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const Home = () => {
     const [allCourse, setAllCourse] = useState([]);
     const [selectedCourse, setSelectedCourse] = useState([]);
-    const [remaining, setRemaining] = useState(0);
+    const [remaining, setRemaining] = useState(20);
     const [totalCredit, setTotalCredit] = useState(0);
 
 
